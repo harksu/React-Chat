24273e461ecb12c242e920a795670ce17e2eb757
harksu <harskulim@naver.com>
2022-06-23 02:13:54 +0900

    todo : firebase version format exchange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import ChatPage from "./components/ChatPage/ChatPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 export default function App() {
- 
   return (
-    <Router>  
-        <Routes>
-          <Route exact path="/" element={<ChatPage/>}/>
-          <Route exact path="/login" element={<LoginPage/>}/>
-          <Route exact path="/register" element={<RegisterPage/>}/>
-        </Routes>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ChatPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+      </Routes>
     </Router>
   );
 }
-
